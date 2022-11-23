46734b8fb9acf05a06cf1673ac1b0f17a3aa94e5
pedroviana71 <pedrohenrique.viana5@gmail.com>
2022-11-22 22:24:32 -0300

    fixed color when 0

--- script.js
+++ script.js
@@ -1,35 +1,37 @@
-let valor = 0
-const update = document.querySelector(".contador")
-const aumentar = document.getElementById("aumentar")
-const diminuir = document.getElementById("diminuir")
-const reset = document.getElementById("reset")
+let valor = 0;
+const update = document.querySelector(".contador");
+const aumentar = document.getElementById("aumentar");
+const diminuir = document.getElementById("diminuir");
+const reset = document.getElementById("reset");
 
-aumentar.addEventListener("click", function() {
-    valor += 1
-    update.textContent = valor
-    if(valor>0){
-        update.style.color = "rgb(129, 204, 122)"
-    }  
-}
-)
+const resetarCor = () => {
+  valor = 0;
+  update.textContent = valor;
+  update.style.color = "#49475B";
+};
 
-diminuir.addEventListener("click", function(){
-    valor -= 1    
-    update.textContent = valor
-    if(valor < 0) {
-        update.style.color = "rgb(211, 132, 132)"
-    }
-    
+aumentar.addEventListener("click", function () {
+  valor += 1;
+  update.textContent = valor;
+  if (valor > 0) {
+    update.style.color = "rgb(129, 204, 122)";
+  }
+  if (valor === 0) {
+    resetarCor();
+  }
+});
 
+diminuir.addEventListener("click", function () {
+  valor -= 1;
+  update.textContent = valor;
+  if (valor < 0) {
+    update.style.color = "rgb(211, 132, 132)";
+  }
+  if (valor === 0) {
+    resetarCor();
+  }
+});
 
-})
-
-reset.addEventListener("click", function(){
-    valor = 0
-    update.textContent = valor
-    if (valor == 0){
-        update.style.color = "#49475B"
-    }
-})
-
-
+reset.addEventListener("click", function () {
+  resetarCor();
+});
